68c18cfb12710f2ebe389916325a5d45cacbdb4b
Austin <hautera@me.com>
2024-03-18 10:58:20 -0700

    Add more programs to download

--- download.sh
+++ download.sh
@@ -1,4 +1,13 @@
 sudo apt-get update
 sudo apt update
 sudo apt-get install i3
+sudo apt-get install i3blocks
+sudo apt-get install feh
 sudo apt install fzf
+# nvim install 
+sudo add-apt-repository ppa:neovim-ppa/stable
+sudo apt-get update
+sudo apt-get install neovim
+
+#packer for nvim
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
