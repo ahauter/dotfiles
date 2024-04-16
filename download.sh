9ac06d5abfa7eb72ce027e57b68d524e5cbbd5ce
Austin <hautera@me.com>
2024-04-16 09:11:52 -0700

    Install dotnet

--- download.sh
+++ download.sh
@@ -41,3 +41,8 @@ sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-ubuntu-$(lsb_release -cs)-prod $(lsb_release -cs) main" > /etc/apt/sources.list.d/dotnetdev.list'
 sudo apt-get update
 sudo apt-get install azure-functions-core-tools-4
+
+echo "Install C#"
+wget https://dot.net/v1/dotnet-install.sh -O dotnet-install.sh
+chmod +x ./dotnet-install.sh
+./dotnet-install.sh --version latest
